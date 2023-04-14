ae5e9117ab12a0d7fd9c5f254de079ef298db23d
XaQ1997 <57348621+XaQ1997@users.noreply.github.com>
2023-04-14 22:29:33 +0200

    Dodano podwersję v0.6.0

---
+++ v0.6.0/unit.py
@@ -0,0 +1,56 @@
+import random
+
+from map import *
+
+class Unit:
+	def __init__(self, team):
+		self.team=team[0]
+		self.max_move=1
+		self.pos=[0, 0]
+		
+		self.hunger=0
+		self.resources=0
+	
+	def move(self, map):
+		for i in range(self.max_move):
+			directions=["stop", "north", "east", "south", "west"]
+			removed=[]
+			
+			if self.pos[0]==0 or map.occupied[self.pos[0]-1][self.pos[1]]!=".":
+				directions.remove("north")
+				removed.append("north")
+			if self.pos[0]==19 or map.occupied[self.pos[0]+1][self.pos[1]]!=".":
+				directions.remove("south")
+				removed.append("south")
+			if self.pos[1]==0 or map.occupied[self.pos[0]][self.pos[1]-1]!=".":
+				directions.remove("west")
+				removed.append("west")
+			if self.pos[1]==19 or map.occupied[self.pos[0]][self.pos[1]+1]!=".":
+				directions.remove("east")
+				removed.append("east")
+			
+			rnd=random.randint(0, len(directions)-1)
+			
+			for i in range(len(removed)):
+				directions.append(removed[i])
+			
+			if rnd!=0:
+				map.pop(self.pos)
+			if directions.index("north")==rnd:
+				map.occupied[self.pos[0]-1][self.pos[1]]=self.team
+				self.pos[0]-=1
+				continue
+			if directions.index("south")==rnd:
+				map.occupied[self.pos[0]+1][self.pos[1]]=self.team
+				self.pos[0]+=1
+				continue
+			if directions.index("west")==rnd:
+				map.occupied[self.pos[0]][self.pos[1]-1]=self.team
+				self.pos[1]-=1
+				continue
+			if directions.index("east")==rnd:
+				map.occupied[self.pos[0]][self.pos[1]+1]=self.team
+				self.pos[1]+=1
+	
+	def gather(self, resources):
+		self.resources+=resources
